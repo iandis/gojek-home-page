8910d8d9a93c8b061aac5e3591aa100536bff26a
Iandi Santulus <iandisantulusn@gmail.com>
2022-06-11 19:51:47 +0700

    feat: add colors

---
+++ lib/theme_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/painting.dart';
+
+abstract class ThemeColors {
+  static const Color primary = Color.fromARGB(255, 0, 136, 12);
+  static const Color darkBlue = Color.fromARGB(255, 0, 129, 160);
+  static const Color peduliLindungiBlue = Color.fromARGB(255, 1, 177, 224);
+  static const Color red = Color.fromARGB(255, 237, 47, 47);
+
+  static const Color white = Color(0xFFFFFFFF);
+}
